37839617d9ebd9e31286337ab450479fe15aa1a9
blackout358 <32801563+blackout358@users.noreply.github.com>
2023-08-07 14:30:16 +0100

    get and send messages

--- lib/logic/chat/chat_service.dart
+++ lib/logic/chat/chat_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mizu/model/message.dart';
 
 class ChatService extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -12,7 +13,41 @@ class ChatService extends ChangeNotifier {
     final String currentUserID = _firebaseAuth.currentUser!.uid;
     final String currentUserEmail = _firebaseAuth.currentUser!.email.toString();
     final Timestamp timestamp = Timestamp.now();
+
+    Message newMessage = Message(
+      senderID: currentUserID,
+      senderEmail: currentUserEmail,
+      recieverID: recieverID,
+      message: message,
+      timestamp: timestamp,
+    );
+
+    List<String> ids = [
+      currentUserID,
+      recieverID,
+    ];
+    ids.sort();
+    String chatRoomID = ids.join('-');
+
+    await _fireStore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .add(newMessage.toMap());
   }
 
   // Get message
+
+  Stream<QuerySnapshot> getMessages(String userID, String otherUserID) {
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join("-");
+
+    return _fireStore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
