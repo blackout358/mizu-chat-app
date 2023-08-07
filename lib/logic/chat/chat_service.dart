a679c0bd116df9824f74d7b863c68239dc8276ea
blackout358 <32801563+blackout358@users.noreply.github.com>
2023-08-07 13:24:32 +0100

    Added message class

---
+++ lib/logic/chat/chat_service.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatService extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  // Send message
+  Future<void> sendMessage(String recieverID, String message) async {
+    // current user info
+    final String currentUserID = _firebaseAuth.currentUser!.uid;
+    final String currentUserEmail = _firebaseAuth.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
+  }
+
+  // Get message
+}
