6bc54624692e7af55ab8f4323ee8dc1845487457
blackout358 <32801563+blackout358@users.noreply.github.com>
2023-08-24 14:20:18 +0100

    Swipe to reply working

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChatBubble extends StatefulWidget {
   final String message;
@@ -24,6 +25,8 @@ class _ChatBubbleState extends State<ChatBubble>
   double dragDistance = 0.0;
   bool animating = false;
   late AnimationController controller;
+  late Animation<double> iconOpacityAnimation;
+  final _key = GlobalKey();
 
   @override
   void initState() {
@@ -32,16 +35,25 @@ class _ChatBubbleState extends State<ChatBubble>
       vsync: this,
       duration: Duration(milliseconds: 200),
     );
+    iconOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
   }
 
   void handleDragEnd() {
     setState(() {
       if (dragDistance >= MediaQuery.of(context).size.width * 0.20) {
         print("Reply");
+        // controller.animateTo(0.99);
+
         // controller.forward();
         controller.reverse();
         print("OffsetX $offsetX");
         print("Drag distance: $dragDistance");
+        // controller.
         offsetX = 0;
         dragDistance = 0;
       } else {
@@ -50,68 +62,101 @@ class _ChatBubbleState extends State<ChatBubble>
 
         print("OffsetX $offsetX");
         print("Drag distance: $dragDistance");
+        // controller.reset();
         offsetX = 0;
         dragDistance = 0;
       }
     });
+    // controller.reverse();
+  }
+
+  double calculateArrowOpacity(double size) {
+    return (offsetX / size).clamp(0.0, 1.0);
   }
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: widget.isSender ? Alignment.centerRight : Alignment.centerLeft,
-      child: GestureDetector(
-        onTap: () {
-          print("OffsetX $offsetX");
-          print("Drag distance: $dragDistance");
-          // widget.onPressed;
-        },
-        onHorizontalDragUpdate: (details) {
-          double delta = details.primaryDelta ?? 0.0;
-          double maxDrag = MediaQuery.of(context).size.width * 0.25;
-          setState(
-            () {
-              offsetX = (offsetX + delta).clamp(-maxDrag, maxDrag);
-              dragDistance += delta.abs();
+      child: Stack(
+        alignment:
+            widget.isSender ? Alignment.centerRight : Alignment.centerLeft,
+        children: [
+          Transform.scale(
+            scaleX: widget.isSender ? 1 : -1,
+            child: Icon(
+              Icons.reply,
+              color: Colors.amberAccent,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              print("OffsetX $offsetX");
+              print("Drag distance: $dragDistance");
+              // widget.onPressed;
             },
-          );
-        },
-        onHorizontalDragCancel: () {
-          handleDragEnd();
-        },
-        onHorizontalDragEnd: (details) {
-          handleDragEnd();
-        },
-        child: AnimatedBuilder(
-          animation: controller,
-          builder: (context, child) {
-            return Transform.translate(
-              offset: Offset(
-                offsetX - (offsetX * controller.value),
-                0.0,
-                // offsetX = 0,
-              ),
-              child: child,
-            );
-          },
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.75,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: widget.colour,
-            ),
-            child: Text(
-              widget.message,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.white,
+            onHorizontalDragUpdate: (details) {
+              double delta = details.primaryDelta ?? 0.0;
+              double maxDrag = 30;
+              // iconOpacityAnimation = (offsetX /
+              //     MediaQuery.of(context).size.width *
+              //     0.25) as Animation<double>;
+              // print(controller);
+              // print(iconOpacityAnimation);
+              setState(
+                () {
+                  offsetX = (offsetX + delta).clamp(-maxDrag, maxDrag);
+                  dragDistance += delta.abs();
+                },
+              );
+              // iconOpacityAnimation = Tween<double>(
+              //   begin: 0.0,
+              //   end: (offsetX / maxDrag).clamp(0.0, 1.0),
+              // ) as Animation<double>;
+            },
+            onHorizontalDragCancel: () {
+              handleDragEnd();
+              // controller.reset();
+              // controller.dispose();
+            },
+            onHorizontalDragEnd: (details) {
+              handleDragEnd();
+              // controller.reset();
+              // controller.dispose();
+            },
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(
+                    offsetX - (offsetX * controller.value),
+                    0.0,
+                    // offsetX = 0,
+                  ),
+                  child: child,
+                );
+              },
+              child: Container(
+                key: _key,
+                padding: const EdgeInsets.all(12),
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.75,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: widget.colour,
+                ),
+                child: Text(
+                  widget.message,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
