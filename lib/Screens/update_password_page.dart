1b2ea34f8a8a84cd79c5e62810999ca05b67e982
blackout358 <32801563+blackout358@users.noreply.github.com>
2023-09-03 13:21:14 +0100

    Cleaned up updatePassword method

--- lib/Screens/update_password_page.dart
+++ lib/Screens/update_password_page.dart
@@ -26,85 +26,136 @@ class UpdatePassword extends StatelessWidget {
     final user = _firebaseAuth.currentUser;
     const double spacing = 15;
 
+    void showSnackBar(String text) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        CustomSnackBar(
+          text: text,
+          textColour: Colors.black,
+          height: 30,
+          duration: 2,
+          fontSize: 20,
+          backgroundColor: Colors.purple[200]!,
+        ),
+      );
+    }
+
+    void showSuccessSnackBar(String text) {
+      showSnackBar(text);
+    }
+
+    void clearControllers() {
+      newPasswordController.clear();
+      confirmPasswordController.clear();
+      confirmOldPasswordController.clear();
+    }
+
     void updatePassword() async {
+      if (user == null) {
+        return;
+      }
       try {
-        if (user != null) {
-          final userCredential = EmailAuthProvider.credential(
-            email: user.email ?? "",
-            password: confirmOldPasswordController.text,
-          );
+        final userCredential = EmailAuthProvider.credential(
+          email: user.email ?? "",
+          password: confirmOldPasswordController.text,
+        );
+        await user.reauthenticateWithCredential(userCredential);
 
-          try {
-            await user.reauthenticateWithCredential(userCredential);
-
-            // Check if the new password is not empty
-            final newPassword = newPasswordController.text;
-            if (newPasswordController.text == confirmPasswordController.text) {
-              if (newPassword.isNotEmpty) {
-                await user.updatePassword(newPassword);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  CustomSnackBar(
-                    text: "Password updated successfully",
-                    textColour: Colors.black,
-                    height: 30,
-                    duration: 2,
-                    fontSize: 20,
-                    backgroundColor: Colors.purple[200]!,
-                  ),
-                );
-                newPasswordController.clear();
-                confirmPasswordController.clear();
-                confirmOldPasswordController.clear();
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  CustomSnackBar(
-                    text: "New password cannot be empty",
-                    textColour: Colors.black,
-                    height: 30,
-                    duration: 2,
-                    fontSize: 20,
-                    backgroundColor: Colors.purple[200]!,
-                  ),
-                );
-              }
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                CustomSnackBar(
-                  text: "Passwords do not match",
-                  textColour: Colors.black,
-                  height: 30,
-                  duration: 2,
-                  fontSize: 20,
-                  backgroundColor: Colors.purple[200]!,
-                ),
-              );
-            }
-          } catch (e) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              CustomSnackBar(
-                text: ErrorCodeHandler.errorCodeDebug(e.toString()),
-                textColour: Colors.black,
-                height: 30,
-                duration: 2,
-                fontSize: 20,
-                backgroundColor: Colors.purple[200]!,
-              ),
-            );
+        if (newPasswordController.text == confirmPasswordController.text) {
+          if (newPasswordController.text.isNotEmpty) {
+            await user.updatePassword(newPasswordController.text);
+            showSuccessSnackBar("Password updated successfully");
+            clearControllers();
+          } else {
+            showSnackBar("New password cannot be empty");
           }
+        } else {
+          showSnackBar("Passwords do not match");
         }
       } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          CustomSnackBar(
-            text: ErrorCodeHandler.errorCodeDebug(e.toString()),
-            textColour: Colors.black,
-            height: 30,
-            duration: 2,
-            fontSize: 20,
-            backgroundColor: Colors.purple[200]!,
-          ),
-        );
+        final errorMessage = ErrorCodeHandler.errorCodeDebug(e.toString());
+        showSnackBar(errorMessage);
       }
     }
+
+    // void updatePassword() async {
+    //   try {
+    //     if (user != null) {
+    //       final userCredential = EmailAuthProvider.credential(
+    //         email: user.email ?? "",
+    //         password: confirmOldPasswordController.text,
+    //       );
+
+    //       try {
+    //         await user.reauthenticateWithCredential(userCredential);
+
+    //         // Check if the new password is not empty
+    //         final newPassword = newPasswordController.text;
+    //         if (newPasswordController.text == confirmPasswordController.text) {
+    //           if (newPassword.isNotEmpty) {
+    //             await user.updatePassword(newPassword);
+    //             ScaffoldMessenger.of(context).showSnackBar(
+    //               CustomSnackBar(
+    //                 text: "Password updated successfully",
+    //                 textColour: Colors.black,
+    //                 height: 30,
+    //                 duration: 2,
+    //                 fontSize: 20,
+    //                 backgroundColor: Colors.purple[200]!,
+    //               ),
+    //             );
+    //             newPasswordController.clear();
+    //             confirmPasswordController.clear();
+    //             confirmOldPasswordController.clear();
+    //           } else {
+    //             ScaffoldMessenger.of(context).showSnackBar(
+    //               CustomSnackBar(
+    //                 text: "New password cannot be empty",
+    //                 textColour: Colors.black,
+    //                 height: 30,
+    //                 duration: 2,
+    //                 fontSize: 20,
+    //                 backgroundColor: Colors.purple[200]!,
+    //               ),
+    //             );
+    //           }
+    //         } else {
+    //           ScaffoldMessenger.of(context).showSnackBar(
+    //             CustomSnackBar(
+    //               text: "Passwords do not match",
+    //               textColour: Colors.black,
+    //               height: 30,
+    //               duration: 2,
+    //               fontSize: 20,
+    //               backgroundColor: Colors.purple[200]!,
+    //             ),
+    //           );
+    //         }
+    //       } catch (e) {
+    //         ScaffoldMessenger.of(context).showSnackBar(
+    //           CustomSnackBar(
+    //             text: ErrorCodeHandler.errorCodeDebug(e.toString()),
+    //             textColour: Colors.black,
+    //             height: 30,
+    //             duration: 2,
+    //             fontSize: 20,
+    //             backgroundColor: Colors.purple[200]!,
+    //           ),
+    //         );
+    //       }
+    //     }
+    //   } catch (e) {
+    //     ScaffoldMessenger.of(context).showSnackBar(
+    //       CustomSnackBar(
+    //         text: ErrorCodeHandler.errorCodeDebug(e.toString()),
+    //         textColour: Colors.black,
+    //         height: 30,
+    //         duration: 2,
+    //         fontSize: 20,
+    //         backgroundColor: Colors.purple[200]!,
+    //       ),
+    //     );
+    //   }
+    // }
 
     return Scaffold(
         appBar: AppBar(
